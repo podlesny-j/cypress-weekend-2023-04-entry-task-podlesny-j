ae0ccde5785b60e589725c2f2f3a81f732f72cd6
Podles <kuba.podlesny@gmail.com>
2023-03-11 20:19:36 +0100

    add getRandomPictureCard custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,3 +8,19 @@ Cypress.Commands.add("suppressCookieAndSubscriptionDialogs", () => {
 Cypress.Commands.add("getDataTest", (selector, ...args) => {
   return cy.get(`[data-test=${selector}]`, ...args)
 })
+
+
+Cypress.Commands.add("getRandomPictureCard", () => {
+  cy.getDataTest("TrendingDestinations")
+    .find("[data-test='PictureCard']")
+    .its("length")
+    .as("numberOfCards")
+
+  cy.step("Choose one picture card at random")
+  cy.then(function () {
+    expect(this.numberOfCards).to.be.gte(1)
+    const randomIndex = Math.floor(Math.random() * this.numberOfCards)
+    cy.log("Randomly chose to click Card number " + (randomIndex + 1))
+    cy.getDataTest("PictureCard").eq(randomIndex).as("randomPictureCard")
+  })
+})
